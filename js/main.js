4f1b95a3330eea23540aaf06b24a9fdbddea1e56
Takumi Nakayama <takumi.n19971228@gmail.com>
2023-11-02 23:40:55 +0900

    削除機能を実装しました

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
 'use strict';
 
 {
-    const todos = [
-        { title: 'aaa', isCompleted: false },
-        { title: 'bbb', isCompleted: false },
-        { title: 'ccc', isCompleted: false },
-    ];
+    let todos;
+
+    if (localStorage.getItem('todos') === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+
+    const saveTodos = () => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }
 
     const rendarTodo = (todo) => {
         /*
@@ -18,6 +24,14 @@
         const input = document.createElement('input');
         input.type = 'checkbox';
         input.checked = todo.isCompleted;
+        input.addEventListener('change', () => {
+            todos.forEach((item) => {
+                if (item.id === todo.id) {
+                    item.isCompleted = !item.isCompleted;
+                }
+            });
+            saveTodos();
+        });
 
         const span = document.createElement('span');
         span.textContent = todo.title;
@@ -28,6 +42,16 @@
 
         const button = document.createElement('button');
         button.textContent = 'x';
+        button.addEventListener('click', () => {
+            if (confirm('Sure?') === false) {
+                return;
+            };
+            li.remove();
+            todos = todos.filter((item) => {
+                return item.id !== todo.id;
+            });
+            saveTodos();
+        });
 
         const li = document.createElement('li');
         li.appendChild(label);
@@ -40,6 +64,36 @@
             rendarTodo(todo);
         });
     };
+
+    document.querySelector('#add-form').addEventListener('submit', (e) => {
+        e.preventDefault();
+        const input = document.querySelector('#add-form input');
+        const todo = {
+            id: Date.now(),
+            title: input.value,
+            isCompleted: false,
+        };
+        rendarTodo(todo);
+        todos.push(todo);
+        console.table(todos);
+        saveTodos();
+        input.value = '';
+        input.focus();
+    });
+
+    document.querySelector('#purge').addEventListener('click', () => {
+        if (confirm('Sure?') === false) {
+            return;
+        };
+        todos = todos.filter((todo) => {
+            return todo.isCompleted === false;
+        });
+        saveTodos();
+        document.querySelectorAll('#todos li').forEach((li) => {
+            li.remove();
+        });
+        rendarTodos();
+    });
 
     rendarTodos();
 }
